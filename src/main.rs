1d789e5473accb08dd2a10f34e578c4b3ff9475e
Casey Capetola <caseycapetola@gmail.com>
2024-07-31 16:17:19 -0400

    Implemented modified version of the 2D grid

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// use rand::Rng;
+use rand::Rng;
 
 // static int POS = 1
 // let POS: bool = true;
@@ -18,12 +18,27 @@ impl Row {
 }
 
 fn main() {
-    println!("Hello, world!");
-    // 0 -> negative spin; 1 -> positive spin
-    // 2D Array -> 
+    let mut rng = rand::thread_rng();
+    let arr_size: i32 = 10;
+    let beta: f32 = 0.5;
+
     let mut pos_grid: Row = Row{
         elements: Vec::new()
     };
-    pos_grid.init_grid(true, 10);
+
+    pos_grid.init_grid(true, arr_size*arr_size);
     println!("{:#?}", pos_grid.elements);
+
+    let mut neg_grid: Row = Row {
+        elements: Vec::new()
+    };
+    neg_grid.init_grid(false, arr_size*arr_size);
+    let mut diff: i32 = arr_size*arr_size;
+
+    let mut rand_num: f32;
+    while diff > 0 {
+        rand_num = rng.gen::<f32>();
+    }
+
+
 }
